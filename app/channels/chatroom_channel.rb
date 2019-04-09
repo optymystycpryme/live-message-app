fffac151dc3241398eaad603ed28c4194f8e5f30
brian haltom <brianwh87@gmail.com>
2019-04-09 10:34:12 -0700

    Integrate broadcasting messages, display messages using partial

---
+++ app/channels/chatroom_channel.rb
@@ -0,0 +1,10 @@
+# Chatroom Channel 
+class ChatroomChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from 'chatroom_channel'
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
